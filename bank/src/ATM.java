09ccc2f03f648c2e61712e2e86240477ec7bcd3d
Raymond-Blok <raymond-blok@outlook.com>
2019-03-18 15:53:28 +0100

    called the exorcist

--- bank/src/ATM.java
+++ bank/src/ATM.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.awt.Button;
 import java.lang.Integer;
 import java.util.concurrent.TimeUnit;
+import java.lang.InterruptedException;
 
 public class ATM {
   private Bank bank;
   private ATMScreen as;
+  private String yes;
   CardReader accountReader;
-  DisplayText text;
+  DisplayText mainText;
+  DisplayText pinText;
   ArrayList<ScreenButton> keypadButtonList;
   ArrayList<ScreenButton> amountButtonList;
   ArrayList<ScreenButton> confirmButtonList;
@@ -30,157 +33,157 @@ public class ATM {
     f.add(as);
     f.setVisible(true);
     // text
-    this.text = new DisplayText("welkomstext", new Point(200, 200));
-    this.as.add(text);
-    text.giveOutput("Insert your card");
+    this.yes = "yes";
+    this.mainText = new DisplayText("mainText", new Point(200, 200));
+
+    this.pinText = new DisplayText("pinText", new Point(200, 250));
     this.accountReader = new CardReader("account");
     this.keypad = new Keypad("keypad");
     this.receiptPrinter = new ReceiptPrinter("receiptPrinter");
     this.keypadButtonList = this.genButtons(20, 20, 3, 3, 40, 40, "0");
     this.amountButtonList = this.genButtons(40, 40, 1, 4, 100, 100, null, new String[]{"\u20AC 10", "\u20AC 20", "\u20AC 50", "\u20AC 100"});
-    this.confirmButtonList = this.genButtons(40, 250, 1, 2, 200, 200, null, new String[]{"e","no"});
-    this.inputDevices = new ArrayList<InputDevice>();
-    for(int i = 0; i < this.keypadButtonList.size(); i++) {
-      this.inputDevices.add(this.keypadButtonList.get(i));
-    };
-    for(int i = 0; i < this.amountButtonList.size(); i++) {
-      this.inputDevices.add(this.amountButtonList.get(i));
-    }
-    for(int i = 0; i < this.confirmButtonList.size(); i++) {
-      this.inputDevices.add(this.confirmButtonList.get(i));
-    }
-    this.inputDevices.add(this.keypad);
+    this.confirmButtonList = this.genButtons(40, 250, 1, 2, 200, 200, null, new String[]{this.yes,"no"});
     while(true) {
       this.doTransaction();
     }
   }
-  public void doTransaction() {
-    String account_text;
+
+  private void doTransaction() {
     Client client;
-    while(true) {
-      do{
-        account_text = this.accountReader.getInput();
-      } while(account_text == null);
-      client = this.bank.get(account_text);
-      if(client != null){
-        break;
-      }
-      this.text.giveOutput("wacht een paar en begin opnieuw...");
-    }
-    for(int i = 0; i < this.keypadButtonList.size(); i++) {
-      this.as.add(this.keypadButtonList.get(i));
-    }
-    this.text.giveOutput("enter your PIN");
-
-    // String keyValue;
-    // keyValue = this.keypad.getInput();
-    // if(keyValue != null) {
-    //   this.text.giveOutput(keyValue);
-    // }
-    int counter = 0;
-    String buttonText;
-    String pinList = "";
-    String xText = "";
-    DisplayText pinText = new DisplayText("pinText", new Point(200, 250));
-    this.as.add(pinText);
-    while(true) {
-      while(true) {
-        buttonText = this.inputDevices.get(counter).getInput();
-        if(buttonText != null) {
-          if( buttonText.equals("\n")){
-            break;
-          }
-          pinList += buttonText;
-          xText += "X";
-          System.out.println(buttonText);
-          pinText.giveOutput(xText);
-        }
-        counter++;
-        if(counter > (this.inputDevices.size()-1)) {
-          counter = 0;
-        }
-      }
-      if(client.checkPin(pinList)) {
-        break;
-      } else {
-        this.text.giveOutput("foute pincode begin opnieuw");
-      }
-      xText = "";
-      pinList = "";
-      pinText.giveOutput(xText);
-    }
-    String balance = Integer.toString(client.getBalance(pinList));
-    this.as.clear();
-    this.as.add(this.text);
-    this.text.giveOutput("youre in!!!");
-    for(int i = 0; i < this.amountButtonList.size(); i++) {
-      this.as.add(this.amountButtonList.get(i));
-    }
-    counter = 0;
-    int amount = 0;
-    String amountText = "";
-    while(true) {
-      while(true) {
-        buttonText = this.inputDevices.get(counter).getInput();
-        if(buttonText != null) {
-          if( buttonText.equals("\n")){
-            amount = Integer.parseInt(amountText);
-            break;
-          }
-          String[] splitString = buttonText.split(" ");
-          if(splitString.length >= 2) {
-            amount = Integer.parseInt(splitString[1]);
-            break;
-          } else {
-            amountText += splitString[0];
-          }
-        }
-        counter++;
-        if(counter > (this.inputDevices.size()-1)) {
-          counter = 0;
-        }
-      }
-      amountText = "";
-      System.out.println(amount);
-      if (client.withdraw(amount, pinList)) {
-        break;
-      } else {
-        this.text.giveOutput("amigo sin dinero");
-      }
-    }
-    this.as.clear();
-    this.text.giveOutput("willst du eine Quittung?");
-    this.as.add(this.text);
-    for(int i = 0; i < this.confirmButtonList.size(); i++) {
-      this.as.add(this.confirmButtonList.get(i));
-    }
-    counter = 0;
-    boolean printReceipt = false;
-    while(true) {
-      buttonText = this.inputDevices.get(counter).getInput();
-      if(buttonText != null) {
-        printReceipt = buttonText.equals("e");
-        break;
-      }
-      counter++;
-      if(counter > (this.inputDevices.size()-1)) {
-        counter = 0;
-      }
-    }
-    this.as.clear();
-    if(printReceipt) {
-      this.receiptPrinter.giveOutput("balance: "+ balance + "\n" + "withdrawn: " + amount + "\n" + "new balance: " + Integer.toString(client.getBalance(pinList)));
-    }
-    this.text.giveOutput("");
-    this.as.clear();
-
-
+    String pinCode;
+    int amount;
+    client = this.getClient();
+    pinCode = this.authenticate(client);
+    this.withdrawMenu(client, pinCode);
   }
 
+  private Client getClient() {
+    Client client;
+    this.as.clear();
+    this.mainText.giveOutput("Insert your card");
+    this.as.add(this.mainText);
+    do {
+      String IBan = this.accountReader.getInput();
+      client = this.bank.get(IBan);
+      if(IBan != null && client == null) {
+        this.mainText.giveOutput("no card found, please try again.");
+      }
+    } while(client == null);
 
-  // private Client getClient(String IBAN) {
-  //
-  // }
+    return client;
+  }
+
+  private String authenticate(Client client) {
+    String pinCode = "";
+    String xes = "";
+    String buttonPressed;
+    ArrayList<InputDevice> inputDevices = new ArrayList<InputDevice>();
+    this.as.clear();
+    this.mainText.giveOutput("Enter your pin");
+    this.as.add(this.mainText);
+    this.as.add(this.pinText);
+    for(int i = 0; i < this.keypadButtonList.size(); i++) {
+      ScreenButton keypadButton = this.keypadButtonList.get(i);
+      inputDevices.add(keypadButton);
+      this.as.add(keypadButton);
+    }
+    inputDevices.add(this.keypad);
+    while(true) {
+      while(true) {
+        this.pinText.giveOutput(xes);
+        buttonPressed = this.listenForInput(inputDevices);
+        if(buttonPressed.equals("\n")) {
+          break;
+        }
+        pinCode += buttonPressed;
+        xes += "x";
+      }
+      if(client.checkPin(pinCode)) {
+        break;
+      }
+      xes = "";
+      pinCode = "";
+      this.mainText.giveOutput("wrong pin please try again.");
+      try{
+        TimeUnit.SECONDS.sleep(2);
+      }catch(InterruptedException e) {
+        System.err.println(e.toString());
+      }
+    }
+    return pinCode;
+  }
+
+  private int withdrawMenu(Client client, String pinCode) {
+    ScreenButton amountButton;
+    String amountText = "";
+    int amount = 0;
+    int balance;
+    ArrayList<InputDevice> inputDevices = new ArrayList<InputDevice>();
+    balance = client.getBalance(pinCode);
+    this.as.clear();
+    this.mainText.giveOutput("Choose amount");
+    this.as.add(this.mainText);
+    for(int i = 0; i < this.amountButtonList.size(); i++) {
+      amountButton = this.amountButtonList.get(i);
+      inputDevices.add(amountButton);
+      this.as.add(amountButton);
+    }
+    while(true){
+      this.mainText.giveOutput("Choose amount");
+      amountText = this.listenForInput(inputDevices);
+      String[] splitString = amountText.split(" ");
+      amount = Integer.parseInt(splitString[1]);
+      amountText = "";
+      if (client.withdraw(amount, pinCode)) {
+        break;
+      } else {
+        this.mainText.giveOutput("amigo sin dinero");
+        try{
+          TimeUnit.SECONDS.sleep(2);
+        }catch(InterruptedException e) {
+          System.err.println("oops");
+        }
+
+      }
+    }
+    if(this.askForReceipt()) {
+      this.printReceipt(balance, amount);
+    }
+    return amount;
+  }
+
+  private void printReceipt(int balance, int amount) {
+    this.receiptPrinter.giveOutput("balance: "+ (balance+amount) + "\n" + "withdrawn: " + amount + "\n" + "new balance: " + balance);
+  }
+
+  private boolean askForReceipt() {
+    ScreenButton confirmButton;
+    String answer;
+    ArrayList<InputDevice> inputDevices = new ArrayList<InputDevice>();
+    this.as.clear();
+    this.mainText.giveOutput("do you want a receipt?");
+    this.as.add(this.mainText);
+    for(int i = 0; i < this.confirmButtonList.size(); i++) {
+      confirmButton = this.confirmButtonList.get(i);
+      inputDevices.add(confirmButton);
+      this.as.add(confirmButton);
+    }
+    answer = this.listenForInput(inputDevices);
+    return answer.equals(this.yes);
+  }
+
+  private String listenForInput(ArrayList<InputDevice> inputDevices) {
+    String input;
+    int counter = 0;
+    do {
+      input = inputDevices.get(counter).getInput();
+
+      if(++counter > (inputDevices.size()-1)) {
+        counter = 0;
+      }
+    } while(input == null);
+    return input;
+  }
 
   private ArrayList<ScreenButton> genButtons(int startX, int startY ,int gridX, int gridY, int gapX, int gapY, String lastButton) {
     ArrayList<ScreenButton> temp = new ArrayList<ScreenButton>();
