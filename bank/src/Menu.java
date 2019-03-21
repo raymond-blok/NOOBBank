7074c2b552aaf20f016da906accbe90580f265dc
Raymond-Blok <raymond-blok@outlook.com>
2019-03-21 10:59:21 +0100

    added the menu class for easy menu creation and management

---
+++ bank/src/Menu.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Menu {
+  private String name;
+  private Menu prev;
+  private ArrayList<DisplayText> texts;
+  private ArrayList<ScreenButton> buttons;
+  private ArrayList<InputDevice> inputDevices;
+
+  Menu(String name, ArrayList<DisplayText> texts, ArrayList<ScreenButton> buttons) {
+    this.name = name;
+    this.prev = null;
+    this.texts = texts;
+    this.buttons = buttons;
+    this.inputDevices = new ArrayList<InputDevice>();
+    for(int i = 0; i < this.buttons.size(); i++) {
+      this.inputDevices.add(this.buttons.get(i));
+    }
+  }
+
+  public void open(ATMScreen screen) {
+    screen.clear();
+    for(int i = 0; i < this.texts.size(); i++) {
+      screen.add(this.texts.get(i));
+    }
+    for(int i = 0; i < this.buttons.size(); i++) {
+      screen.add(this.buttons.get(i));
+    }
+  }
+
+  public void open(ATMScreen screen, Menu prev) {
+    this.prev = prev;
+    this.open(screen);
+  }
+
+  public void addInputDevice(InputDevice device) {
+    this.inputDevices.add(device);
+  }
+
+  public String listenForInput() {
+    String input;
+    int counter = 0;
+    do {
+      input = this.inputDevices.get(counter).getInput();
+
+      if(counter++ > (this.inputDevices.size()-1)) {
+        counter = 0;
+      }
+    } while(input == null);
+    return input;
+  }
+
+  public Menu getPrev() {
+    return this.prev;
+  }
+
+
+}
